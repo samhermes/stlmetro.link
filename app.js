ac45ce2d8685953b964bcefddced78bab66ed2a2
Sam Hermes <samhermes@me.com>
2016-01-20 18:34:21 -0600

    Possibly this???

--- app.js
+++ app.js
@@ -136,15 +136,43 @@ $('#start-over').click(function() {
 
 
 
-$('header').addEventListener('touchmove', function( event ){ event.preventDefault() };
-$('#station-select h1').addEventListener('touchmove', function( event ){ event.preventDefault() };
 
-$('.station-scroll').addEventListener('touchstart', function( event ){
-    if( this.scrollTop === 0 ) {
-        this.scrollTop += 1;
-    } else if( this.scrollTop + this.offsetHeight >= this.scrollHeight ) {
-        this.scrollTop -= 1;
+
+// Detects if element has scroll bar
+    $.fn.hasScrollBar = function() {
+        return this.get(0).scrollHeight > this.outerHeight();
     }
-}
 
+    $(document).on("touchstart", function(e) {
+        var $scroller;
+        var $target = $(e.target);
 
+        // Get which element could have scroll bars
+        if($target.hasScrollBar()) {
+            $scroller = $target;
+        } else {
+            $scroller = $target
+                .parents()
+                .filter(function() {
+                    return $(this).hasScrollBar();
+                })
+                .first()
+            ;
+        }
+
+        // Prevent if nothing is scrollable
+        if(!$scroller.length) {
+            e.preventDefault();
+        } else {
+            var top = $scroller[0].scrollTop;
+            var totalScroll = $scroller[0].scrollHeight;
+            var currentScroll = top + $scroller[0].offsetHeight;
+
+            // If at container edge, add a pixel to prevent outer scrolling
+            if(top === 0) {
+                $scroller[0].scrollTop = 1;
+            } else if(currentScroll === totalScroll) {
+                $scroller[0].scrollTop = top - 1;
+            }
+        }
+    });
